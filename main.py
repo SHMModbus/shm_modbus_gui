f452eb0ec6b80a989633aa36305acf72762400c8
Nikolas Koesling <nikolas@koesling.info>
2024-03-23 21:22:14 +0100

    add --version

--- main.py
+++ main.py
@@ -3,9 +3,21 @@
 import sys
 from MainWindow import MainWindow
 from PySide6 import QtWidgets
+import argparse
+
+import constants
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser(constants.APP_NAME,
+                                     "Graphical interfaces for shared memory modbus tools to simulate a modbus client")
+    parser.add_argument("--version", help="print version and exit", action="store_true")
+    args = parser.parse_args()
+
+    if args.version:
+        print(f"{constants.APP_NAME} {constants.VERSION}")
+        exit(0)
+
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
