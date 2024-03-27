1534a6c34e4529fd19cb47ff9f4b58915ce1f371
Nikolas Koesling <nikolas@koesling.info>
2024-03-27 21:08:51 +0100

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 setup(
     name='shm-modbus-gui',
-    version='1.0.1',
+    version='1.0.2',
     description='gui for shm-modbus',
     license='GPLv3',
     author='Nikolas Koesling',
